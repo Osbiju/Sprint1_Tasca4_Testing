069ea3deb023e6c8554fe3b2552d726d2ee04312
Osbiju <okeshik@protonmail.com>
2023-11-27 08:16:17 +0100

    ok, repasar per entregar

--- src/main/java/n1exercici3/Error_ArrayIndexOutOfBoundsException.java
+++ src/main/java/n1exercici3/Error_ArrayIndexOutOfBoundsException.java
@@ -1,8 +1,14 @@
 package n1exercici3;
 
 public class Error_ArrayIndexOutOfBoundsException {
+    public static void main(String[] args) {
 
-    void provocarErrorTest() {
+
+        provocarErrorTest();
+
+    }
+
+    static void provocarErrorTest() {
         int[] array = new int[5];
         array[5] = 0;//provoco error
     }
